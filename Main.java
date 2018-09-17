2dfd2ef4652712e4c12cc3a6677c4ad9759ce646
Vrushit Bharucha <34651962+vrushit@users.noreply.github.com>
2018-09-17 17:19:59 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,31 @@
+// Main Java File
+
+
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+
+	
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/application/MainFile.fxml"));
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Media Player");
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
